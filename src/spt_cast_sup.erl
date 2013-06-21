5196732a15a46b2fb594c28d7af50d3b5f8ef2ec
ianliao <lcfillmyheart@163.com>
2013-06-21 09:35:20 +0800

    添加进程组广播模块代码

---
+++ src/spt_cast_sup.erl
@@ -0,0 +1,25 @@
+%%% ==================================================================
+%%% @author:ianliao
+%%% @doc 管理一个进程列表，并向其中每个进程发送消息.
+%%% @end
+%%% ==================================================================
+
+-module(spt_cast_sup).
+-behaviour(supervisor).
+
+%% API
+-export([init/1, start_caster/1]).
+
+-define(MAX_RESTART, 5000000).
+-define(MAX_TIME, 60).
+
+%% 开启一个连接服务进程.
+start_caster(Cast) ->
+  supervisor:start_child(?MODULE, [Cast]).
+  
+init([]) ->
+  {ok,
+   {_SupFlags = {simple_one_for_one, ?MAX_RESTART, ?MAX_TIME},
+    [{undefined, {spt_cast, start_link, []}, transient, 2000, worker, []}]
+   }
+  }.
