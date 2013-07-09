fd8ce37cd9f8a6be1a28e74b5a7361098ba0f358
Allen Zhuo <zhuoyikang@gmail.com>
2013-07-09 12:21:28 +0800

    增加term和binary转化函数

--- src/spt_atom.erl
+++ src/spt_atom.erl
@@ -1,5 +1,6 @@
 -module(spt_atom).
 -export([atom_suffix/2, atom_prefix/2]).
+-export([binary_to_term/1, term_to_binary/1]).
 
 atom_suffix(Table, Suffix) when is_list(Suffix)->
   L = atom_to_list(Table) ++ "_" ++ Suffix,
@@ -15,3 +16,23 @@ atom_prefix(Key, Prefix) when is_list(Prefix)->
 atom_prefix(Key, Prefix) ->
   L = atom_to_list(Prefix) ++ "_" ++ atom_to_list(Key),
   list_to_atom(L).
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% term和binary转换.
+
+binary_to_term(<<Bin/binary>>) ->
+  String = erlang:binary_to_list(Bin),
+  TmpString1 = string:strip(String, both, $.),
+  TmpString = string:strip(TmpString1, both, $"),
+  NewString = TmpString ++ ".",
+  {ok, Tokens,_EndLine} = erl_scan:string(NewString),
+  case erl_parse:parse_exprs(Tokens) of
+    {ok, AbsForm} -> {value, Value,_Bs} = erl_eval:exprs(AbsForm, erl_eval:new_bindings()), Value;
+    _ -> io:format("string error ~p~n", [String]), ok
+  end.
+
+term_to_binary(Term) ->
+  R = io_lib:format("~p", [Term]),
+  S = lists:flatten(R),
+  list_to_binary(S).
