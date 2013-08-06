66bfc913a2e5197aa209079bf01c13753b13985b
Allen Zhuo <zhuoyikang@gmail.com>
2013-08-06 19:52:32 +0800

    new

--- src/spt_atom.erl
+++ src/spt_atom.erl
@@ -1,6 +1,7 @@
 -module(spt_atom).
--export([atom_suffix/2, atom_prefix/2]).
+-export([atom_suffix/2, atom_prefix/2, opt/3]).
 -export([binary_to_term/1, term_to_binary/1]).
+-export([string_to_term/1, term_to_string/1]).
 -export([mysql_escape/1, mysql_unescape/1]).
 
 atom_suffix(Table, Suffix) when is_list(Suffix)->
@@ -18,12 +19,22 @@ atom_prefix(Key, Prefix) ->
   L = atom_to_list(Prefix) ++ "_" ++ atom_to_list(Key),
   list_to_atom(L).
 
-
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% term和binary转换.
 
 binary_to_term(<<Bin/binary>>) ->
-  String = erlang:binary_to_list(Bin),
+  S = erlang:binary_to_list(Bin),
+  string_to_term(S).
+
+term_to_binary(Term) ->
+  S = term_to_string(Term),
+  list_to_binary(S).
+
+%% term和字符串的转换.
+term_to_string(Term) ->
+  R = io_lib:format("~p", [Term]),
+  lists:flatten(R).
+
+string_to_term(String) ->
   TmpString1 = string:strip(String, both, $.),
   TmpString = string:strip(TmpString1, both, $"),
   NewString = TmpString ++ ".",
@@ -33,16 +44,17 @@ binary_to_term(<<Bin/binary>>) ->
     _ -> io:format("string error ~p~n", [String]), ok
   end.
 
-term_to_binary(Term) ->
-  R = io_lib:format("~p", [Term]),
-  S = lists:flatten(R),
-  list_to_binary(S).
-
-
-
 %% 对binary进行escape操作.
 mysql_escape(<<Binary/binary>>) ->
   binary:replace(Binary, <<"\"">>, <<"\\">>, [global, {insert_replaced,1}]).
 
 mysql_unescape(<<Binary/binary>>) ->
   binary:replace(Binary, <<"\\">>, <<"">>, [global]).
+
+%% 参数解析.
+opt(Op, [{Op, Value}|_], _V) ->
+  Value;
+opt(Op, [_|Options], _V) ->
+  opt(Op, Options, _V);
+opt(_, [], V) ->
+  V.
